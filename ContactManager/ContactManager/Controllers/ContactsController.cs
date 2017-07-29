432e74cb19c3547bbdf383c6c58b18e59367f46f
Ken LeFebvre <kenneth@lefebvre.us>
2017-07-29 16:22:05 -0400

    Skeleton project based on Authorization tutorial here: https://docs.microsoft.com/en-us/aspnet/core/security/authorization/secure-data

---
+++ ContactManager/ContactManager/Controllers/ContactsController.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ContactManager.Authorization;
+
+namespace ContactManager.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ContactsController(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _authorizationService = authorizationService;
+            _userManager = userManager;
+        }
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contact.ToListAsync());
+        }
+
+        // GET: Contacts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact
+                .SingleOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContactId,Name,Address,City,State,Zip,Email")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contact.OwnerId = _userManager.GetUserId(User);
+
+                if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Create)))
+                    return new ChallengeResult();
+
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact.SingleOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Update)))
+                return new ChallengeResult();
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContactId,Name,Address,City,State,Zip,Email")] Contact contact)
+        {
+            if (id != contact.ContactId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Update)))
+                    return new ChallengeResult();
+
+                if (contact.Status == ContactStatus.Approved)
+                {
+                    // If the contact is being approved, or updated after approval, the user must have Approval permission.
+                    if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Approve)))
+                        contact.Status = ContactStatus.Submitted;
+                }
+
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.ContactId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact
+                .SingleOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Delete)))
+                return new ChallengeResult();
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contact = await _context.Contact.SingleOrDefaultAsync(m => m.ContactId == id);
+
+            if (!(await _authorizationService.AuthorizeAsync(User, contact, ContactOperations.Delete)))
+                return new ChallengeResult();
+
+            _context.Contact.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contact.Any(e => e.ContactId == id);
+        }
+    }
+}
---
+++ ContactManager/ContactManager/Data/Migrations/20170729190941_OwnerId_Status.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Data.Migrations
+{
+    public partial class OwnerId_Status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Contact",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Contact",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Contact");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Contact");
+        }
+    }
+}
